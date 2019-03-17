abb1b9bb35372eacdef5495288490289f326f8ad
william_harris22 <williamharris_8@hotmail.co.uk>
2019-03-17 14:03:35 +0000

    clean architecture progress check-in

---
+++ src/CleanArchitecture.Core/Entities/Team.cs
@@ -0,0 +1,11 @@
+﻿namespace CleanArchitecture.Core.Entities
+{
+    public class Team
+    {
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public string NickName { get; set; }
+    }
+}
